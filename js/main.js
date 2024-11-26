e467684ee4bb304a0244f6928535422bf7d9cb6d
Thainara88 <thainara.franca@escola.pr.gov.br>
2024-11-26 00:21:35 +0000

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var titulo = document.querySelector("h1");
+titulo.textContent = "Thainara Nutricionista";
+
+var subtitulo = document.querySelector("h2");
+subtitulo.textContent = "Pacientes Cadrastados";
+var paciente = document.querySelector("#primeiro-paciente");
+console.log(paciente);
